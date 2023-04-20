30b848c6d639e38efcfe6c2973a9dcdd1f25ca1e
TelezaKanthonga <telezakanthonga@gmail.com>
2023-04-20 16:33:15 -0700

    4 bit counter

--- counter_and_tb/counter.v
+++ counter_and_tb/counter.v
@@ -1,15 +1,21 @@
-module counter
-(
-  input clk,
-  input rst,
-  input enable,
-  output reg [3:0] out
+// This module is a simple counter that increments its output on each positive edge of the clock signal
+// The output is reset to zero on a positive edge of the reset signal
+// The counter can be enabled or disabled by the enable signal
+
+module counter (
+  input clk,      // Clock input signal
+  input rst,      // Reset input signal
+  input enable,   // Enable input signal
+  output reg [3:0] out   // Output of the counter, a 4-bit binary value
 );
 
-always @(posedge clk)
-begin
-  if (rst)
-    out <= 0;
-  else if (enable)
-    out <= out + 1;
-end
+  always @(posedge clk) // The always block executes on each positive edge of the clock signal
+  begin
+    if (rst)  // Reset the output to zero on a positive edge of the reset signal
+      out <= 0;
+    else if (enable)  // If the counter is enabled, increment the output by one
+      out <= out + 1;
+  end
+
+endmodule
+
